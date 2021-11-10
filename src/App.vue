b04cf4cddcbb32d6ac5c6eb995d85936d2e896ab
Mattp <matt@matt.com>
2021-11-10 14:04:44 -0600

    add lab 10 files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <h1>Would You Rather?</h1>
+  
+  <!-- We are inside WouldYouRather, thus using its props as values. -->
+  <would-you-rather
+    v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged">
+  </would-you-rather>
+  
+  <p>{{ userSelectionMessage }}</p>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+      wyrQuestion: 'Would you rather live in a house shaped like a circle or a triangle?',
+      wyrAnswer1: 'Triangle House',
+      wyrAnswer2: 'Circle House',
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks! You chose ${choice}!`
+    }
   }
 }
 </script>
